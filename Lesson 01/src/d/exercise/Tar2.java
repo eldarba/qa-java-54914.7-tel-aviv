e27f90fe49dbbc24b5f28cddc048832eb0ccfecd
Eldar <eldarba@gmail.com>
2020-10-14 15:36:31 +0300

    aaa

--- Lesson 01/src/d/exercise/Tar2.java
+++ Lesson 01/src/d/exercise/Tar2.java
@@ -3,7 +3,15 @@ package d.exercise;
 public class Tar2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// create 2 random numbers in the range 0 - 10
+		// the numbers are the sides of a rectangle
+		// print the sides
+		// length: 9
+		// width: 3
+		
+		// print the area
+		// print the perimeter
 
 	}
 
